02a2ffdfb17dd47901fd13374d14b424c0ae7a19
M-Matloob-Ahmed <166194505+M-Matloob-Ahmed@users.noreply.github.com>
2024-05-19 23:40:44 +0500

    Add files via upload

---
+++ SpeakGoodBye.js
@@ -0,0 +1,10 @@
+
+( function(window){
+    var byeSpeaker = {}
+      byeSpeaker.speakWord= "Good bye";
+    
+      byeSpeaker.speak= function(name){
+       console.log(byeSpeaker.speakWord + " " + name);
+      }
+      window.byeSpeaker=byeSpeaker;
+      })(window);
